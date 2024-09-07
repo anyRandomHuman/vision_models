c0ceb164f45e0804ff3b37c8ea9885a0c1ff9f84
xiaogangjia <jia266163@gmail.com>
2024-09-07 15:18:04 +0200

    fixed bugs of yolo

--- detectors/yolo.py
+++ detectors/yolo.py
@@ -1,13 +1,13 @@
 from ultralytics import YOLO
 import torch
 import numpy as np
-from obj_detector import Object_Detector
+from detectors.obj_detector import Object_Detector
 
 
 class Yolo_Detrector(Object_Detector):
     def __init__(self, path, to_tensor, device) -> None:
         super().__init__(path=path, to_tensor=to_tensor, device=device)
-        
+
         self.model = YOLO(path)
         self.to_tensor = to_tensor
         self.device = device
@@ -19,11 +19,12 @@ class Yolo_Detrector(Object_Detector):
     def track(self, img):
         self.img = img
         self.prediction = self.model.track(img, persist=True)
-        
-        
+
     def get_mask_feature(self):
-        p = self.prediction[0]
-        return p.masks.data
+        # no mask, return box instead
+        # p = self.prediction[0]
+        # return p.masks.data
+        return self.get_box_feature()
 
     def get_box_feature(self):
         # igonre top_n, always retrive all
@@ -49,21 +50,10 @@ class Yolo_Detrector(Object_Detector):
         else:
             return box.numpy(box)
 
-    def joint_feature(self, features):
-        if self.to_tensor:
-            joint_mask = torch.zeros(features.shape[1:])
-            for i in range(features.shape[0]):
-                joint_mask = torch.logical_or(joint_mask, features[i, :, :])
-        if not self.to_tensor:
-            joint_mask = np.zeros(features.shape[1:])
-            for i in range(features.shape[0]):
-                joint_mask = np.logical_or(joint_mask, features[i, :, :])
-        return joint_mask
-
-    def get_masked_img(self):
-        return self.prediction[0].plot(
-            labels=False, boxes=False, conf=False, probs=False, color_mode="class"
-        )
+        # def get_masked_img(self):
+        #     return self.prediction[0].plot(
+        #         labels=False, boxes=False, conf=False, probs=False, color_mode="class"
+        #     )
         # if self.to_tensor:
         #     feature = torch.from_numpy(feature)
         # img = np.where(
--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 import cv2
+
 # import hydra
 import numpy as np
 
 from omegaconf import DictConfig, OmegaConf
+
 # import ultralytics
-from detectors import detic
+# from detectors import detic
 from detectors.obj_detector import Object_Detector
+from detectors.yolo import Yolo_Detrector
 import h5py
 import time
 from tqdm import tqdm
+
 
 def segment_all_imgs(path: Path, detector, outpath, task):
     traj = path.name
@@ -22,12 +26,15 @@ def segment_all_imgs(path: Path, detector, outpath, task):
             masked_img,
         )
 
-def predict_imgs_from_hdf5(path: Path, detector:Object_Detector, outpath, task, file='imgs.hdf5'):
+
+def predict_imgs_from_hdf5(
+    path: Path, detector: Object_Detector, outpath, task, file="imgs.hdf5"
+):
     traj = path.name
-    f = h5py.File(path / file, 'r')
+    f = h5py.File(path / file, "r")
     out_traj = Path(f"{outpath}/{type(detector)}/{task}/{traj}")
     out_traj.mkdir(parents=True, exist_ok=True)
-    
+
     total_time = 0
     num_imgs = 0
     for key in f.keys():
@@ -35,24 +42,27 @@ def predict_imgs_from_hdf5(path: Path, detector:Object_Detector, outpath, task, 
         out_cam.mkdir(exist_ok=True)
         i = 0
         for img_code in tqdm(f[key]):
-            
+
             img = cv2.imdecode(img_code, 1)
-            
+
             t = time.time()
             detector.predict(img)
             total_time += time.time() - t
             mask = detector.get_mask_feature()
             union_mask = detector.joint_feature(mask)
             masked_img = detector.get_masked_img(union_mask)
-            cv2.imwrite(f'{str(out_cam)}/{i}.jpg', masked_img)
-            
-            i+=1
+            cv2.imwrite(f"{str(out_cam)}/{i}.jpg", masked_img)
 
-        num_imgs +=i
-    time_record = out_traj / 'time.txt'
-    with time_record.open('w') as time_file:
-        time_file.write(f'totel time: {total_time}\n img num: {num_imgs}\n avg:{total_time/(num_imgs)}')
+            i += 1
+
+        num_imgs += i
+    time_record = out_traj / "time.txt"
+    with time_record.open("w") as time_file:
+        time_file.write(
+            f"totel time: {total_time}\n img num: {num_imgs}\n avg:{total_time/(num_imgs)}"
+        )
     f.close()
+
 
 # @hydra.main(config_path="configs", config_name="pick_placing_config.yaml")
 # def main(cfg: DictConfig):
@@ -64,20 +74,37 @@ def predict_imgs_from_hdf5(path: Path, detector:Object_Detector, outpath, task, 
 
 
 if __name__ == "__main__":
-    
+
     img = cv2.imread("0.png")
 
-    obj_det = detic.Detectron(to_tensor=False, classes=['banana', 'cup', 'carrot', 'saucepan', 'hair_dryer', 'bolt', 'hinge', 'bolt'])
-    
+    # obj_det = detic.Detectron(
+    #     to_tensor=False,
+    #     classes=[
+    #         "banana",
+    #         "cup",
+    #         "carrot",
+    #         "saucepan",
+    #         "hair_dryer",
+    #         "bolt",
+    #         "hinge",
+    #         "bolt",
+    #     ],
+    # )
+
+    obj_det = Yolo_Detrector(
+        path="/home/alr_admin/david/praktikum/d3il_david/detector_models/yolov8n.pt",
+        to_tensor=False,
+        device="cuda",
+    )
     obj_det.predict(img)
-    # mask = obj_det.get_mask_feature()
-    # union_mask = obj_det.joint_feature(mask)
-    # masked_img = obj_det.get_masked_img(union_mask)
-    output = obj_det.get_visualized_imgs()
+    mask = obj_det.get_mask_feature()
+    union_mask = obj_det.joint_feature(mask)
+    output = obj_det.get_masked_img(union_mask)
+    # output = obj_det.get_masked_img(masked_img)
+    # output = obj_det.get_visualized_imgs()
     # mask = np.expand_dims(mask, -1)
     cv2.imwrite("output0.jpg", output)
-    
-    
+
     # path = Path('/home/i53/student/qwei/alr/data/pickPlacing/2024_08_05-13_22_36')
     # outpath = '/home/i53/student/qwei/alr/prediction_results'
     # detector = detic.Detectron(to_tensor=False)
